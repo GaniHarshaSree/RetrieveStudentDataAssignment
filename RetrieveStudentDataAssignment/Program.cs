92834413a3c281ca66b7c018a82fe2ce0846b694
GaniHarshaSree <ganiharshasree18@gmail.com>
2023-10-23 17:41:45 +0530

    Add project files.

---
+++ RetrieveStudentDataAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace RetrieveStudentDataAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                String filepath = @"C:\Users\GANI HARSHA SREE\source\repos\RetrieveStudentDataAssignment\RetrieveStudentDataAssignment\StudentRetrieveData.txt"; 
+
+                string[] file_data = File.ReadAllLines(filepath);
+                foreach (var line in file_data)
+                {
+                    Console.WriteLine(line);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
